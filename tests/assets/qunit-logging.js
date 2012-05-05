e4128ee97836f6165931f100399be24dbf2dbe12
Andrew Rowls <andrew@eternicode.com>
2012-05-05 15:33:06 -0400

    Add phantomjs support to unit tests (CLI-capable!)

---
+++ tests/assets/qunit-logging.js
@@ -0,0 +1,28 @@
+// Dummy logging calls (ie, if tests are run in IE)
+window.console = window.console || {};
+window.console.log = window.console.log || function(){};
+window.console.debug = window.console.debug || function(){};
+window.console.info = window.console.info || function(){};
+window.console.warn = window.console.warn || function(){};
+window.console.error = window.console.error || function(){};
+
+(function() {
+   var testName;
+
+   //arg: { name }
+    QUnit.testStart = function(t) {
+        testName = t.name;
+    };
+
+    //arg: { name, failed, passed, total }
+    QUnit.testDone = function(t) {
+        if (t.failed)
+            console.log('Test "' + t.name + '" completed: ' + (0 === t.failed ? 'pass' : 'FAIL') + '\n')
+    };
+
+    //{ result, actual, expected, message }
+    QUnit.log = function(t) {
+        if (!t.result)
+            console.log('Test "' + testName + '" assertion failed. Expected <' + t.expected + '> Actual <' + t.actual + '>' + (t.message ? ': \'' + t.message + '\'' : ''));
+    };
+}());
