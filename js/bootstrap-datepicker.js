e75f4921e17f56e09fb5e221ac4e2da0b4999c0a
Andrew Rowls <andrew@eternicode.com>
2012-07-06 19:16:25 -0400

    Restrict keyboard navigation when startDate/endDate are set

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -515,6 +515,10 @@
 			return this.moveMonth(date, dir*12);
 		},
 
+		dateWithinRange: function(date){
+			return date >= this.startDate && date <= this.endDate;
+		},
+
 		keydown: function(e){
 			if (this.picker.is(':not(:visible)')){
 				if (e.keyCode == 27) // allow escape to hide and re-show picker
@@ -522,7 +526,8 @@
 				return;
 			}
 			var dateChanged = false,
-				dir, day, month;
+				dir, day, month,
+				newDate, newViewDate;
 			switch(e.keyCode){
 				case 27: // escape
 					this.hide();
@@ -532,37 +537,49 @@
 				case 39: // right
 					dir = e.keyCode == 37 ? -1 : 1;
 					if (e.ctrlKey){
-						this.date = this.moveYear(this.date, dir);
-						this.viewDate = this.moveYear(this.viewDate, dir);
+						newDate = this.moveYear(this.date, dir);
+						newViewDate = this.moveYear(this.viewDate, dir);
 					} else if (e.shiftKey){
-						this.date = this.moveMonth(this.date, dir);
-						this.viewDate = this.moveMonth(this.viewDate, dir);
+						newDate = this.moveMonth(this.date, dir);
+						newViewDate = this.moveMonth(this.viewDate, dir);
 					} else {
-						this.date.setDate(this.date.getDate() + dir);
-						this.viewDate.setDate(this.viewDate.getDate() + dir);
+						newDate = new Date(this.date);
+						newDate.setDate(this.date.getDate() + dir);
+						newViewDate = new Date(this.viewDate);
+						newViewDate.setDate(this.viewDate.getDate() + dir);
 					}
-					this.setValue();
-					this.update();
-					e.preventDefault();
-					dateChanged = true;
+					if (this.dateWithinRange(newDate)){
+						this.date = newDate;
+						this.viewDate = newViewDate;
+						this.setValue();
+						this.update();
+						e.preventDefault();
+						dateChanged = true;
+					}
 					break;
 				case 38: // up
 				case 40: // down
 					dir = e.keyCode == 38 ? -1 : 1;
 					if (e.ctrlKey){
-						this.date = this.moveYear(this.date, dir);
-						this.viewDate = this.moveYear(this.viewDate, dir);
+						newDate = this.moveYear(this.date, dir);
+						newViewDate = this.moveYear(this.viewDate, dir);
 					} else if (e.shiftKey){
-						this.date = this.moveMonth(this.date, dir);
-						this.viewDate = this.moveMonth(this.viewDate, dir);
+						newDate = this.moveMonth(this.date, dir);
+						newViewDate = this.moveMonth(this.viewDate, dir);
 					} else {
-						this.date.setDate(this.date.getDate() + dir * 7);
-						this.viewDate.setDate(this.viewDate.getDate() + dir * 7);
+						newDate = new Date(this.date);
+						newDate.setDate(this.date.getDate() + dir * 7);
+						newViewDate = new Date(this.viewDate);
+						newViewDate.setDate(this.viewDate.getDate() + dir * 7);
 					}
-					this.setValue();
-					this.update();
-					e.preventDefault();
-					dateChanged = true;
+					if (this.dateWithinRange(newDate)){
+						this.date = newDate;
+						this.viewDate = newViewDate;
+						this.setValue();
+						this.update();
+						e.preventDefault();
+						dateChanged = true;
+					}
 					break;
 				case 13: // enter
 					this.hide();
