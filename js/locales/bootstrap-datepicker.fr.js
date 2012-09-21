786e26b4fe7dd9eb9a4dc795cab755c73a76f8aa
Stéphane Raimbault <stephane.raimbault@gmail.com>
2012-09-21 11:51:22 +0200

    Fix translation of French months
    Official translation of French months (followed by glibc, GNOME and
    so on). See http://fr.wikipedia.org/wiki/Mois.

--- js/locales/bootstrap-datepicker.fr.js
+++ js/locales/bootstrap-datepicker.fr.js
@@ -5,10 +5,10 @@
 ;(function($){
 	$.fn.datepicker.dates['fr'] = {
 		days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
-		daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"],
+		daysShort: ["Dim.", "Lun.", "Mar.", "Mer.", "Jeu.", "Ven.", "Sam.", "Dim."],
 		daysMin: ["D", "L", "Ma", "Me", "J", "V", "S", "D"],
 		months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
-		monthsShort: ["Jan", "Fév", "Mar", "Avr", "Mai", "Jui", "Jul", "Aou", "Sep", "Oct", "Nov", "Déc"],
+		monthsShort: ["Janv.", "Févr.", "Mars", "Avril", "Mai", "Juin", "Juil.", "Août", "Sept.", "Oct.", "Nov.", "Déc."],
 		today: "Aujourd'hui",
 		clear: "Effacer",
 		weekStart: 1,
