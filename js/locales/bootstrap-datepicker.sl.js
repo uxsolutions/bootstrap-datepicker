b43473d3fdb126d7ac85ad92c37835449030bc71
grudolf <gregor.rudolf@gmail.com>
2012-10-02 20:21:34 +0200

    Added today key to localization.

--- js/locales/bootstrap-datepicker.sl.js
+++ js/locales/bootstrap-datepicker.sl.js
@@ -8,6 +8,7 @@
 		daysShort: ["Ned", "Pon", "Tor", "Sre", "Čet", "Pet", "Sob", "Ned"],
 		daysMin: ["Ne", "Po", "To", "Sr", "Če", "Pe", "So", "Ne"],
 		months: ["Januar", "Februar", "Marec", "April", "Maj", "Junij", "Julij", "Avgust", "September", "Oktober", "November", "December"],
-		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Avg", "Sep", "Okt", "Nov", "Dec"]
+		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Avg", "Sep", "Okt", "Nov", "Dec"],
+		today: "Danes"
 	};
 }(jQuery));
