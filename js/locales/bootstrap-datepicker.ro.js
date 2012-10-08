7ffcf20264ea61d112bfa159c5d7e722c6b952cd
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2012-10-08 19:59:55 +0300

    Added Romanian translation

---
+++ js/locales/bootstrap-datepicker.ro.js
@@ -0,0 +1,14 @@
+/**
+ * Romanian translation for bootstrap-datepicker
+ * Ionut-Cristian Florescu <ionut.florescu@gmail.com>
+ */
+;(function($){
+	$.fn.datepicker.dates['ro'] = {
+		days: ["Duminică", "Luni", "Marți", "Miercuri", "Joi", "Vineri", "Sâmbătă", "Duminică"],
+		daysShort: ["Dum", "Lun", "Mar", "Mie", "Joi", "Vin", "Sâm", "Dum"],
+		daysMin: ["D", "L", "Ma", "Mi", "J", "V", "S", "D"],
+		months: ["Ianuarie", "Februarie", "Martie", "Aprilie", "Mai", "Iunie", "Iulie", "August", "Septembrie", "Octombrie", "Noiembrie", "Decembrie"],
+		monthsShort: ["Ian", "Feb", "Mar", "Apr", "Mai", "Iun", "Iul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+		today: "Astăzi"
+	};
+}(jQuery));
