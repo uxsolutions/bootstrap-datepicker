df258e2fd5869d0f7c82488a5db95e2575916fe1
Albert Bellonch Llargués <albert@itnig.net>
2012-12-12 21:36:03 +0100

    Add Catalan locale

---
+++ js/locales/bootstrap-datepicker.ca.js
@@ -0,0 +1,14 @@
+/**
+ * Catalan translation for bootstrap-datepicker
+ * Albert bellonch <albert@itnig.net>
+ */
+;(function($){
+	$.fn.datepicker.dates['ca'] = {
+		days: ["Diumenge", "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte"],
+    daysShort: ["Diu", "Dll", "Dmt", "Dmc", "Dij", "Div", "Dis", "Diu"],
+		daysMin: ["Dg", "Dl", "Dt", "Dc", "Dj", "Dv", "Ds", "Dg"],
+		months: ["Gener", "Febrer", "Març", "Abril", "Maig", "Juny", "Juliol", "Agost", "Setembre", "Octubre", "Novembre", "Desembre"],
+		monthsShort: ["Gen", "Feb", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Des"],
+		today: "Avui"
+	};
+}(jQuery));
