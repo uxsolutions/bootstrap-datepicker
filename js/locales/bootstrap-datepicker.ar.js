a7dfcd567f9212c3c4f33954727b48a37184d1fb
Mohammed Alshehri <alshehri866@gmail.com>
2013-05-12 14:43:19 +0300

    Create bootstrap-datepicker.ar.js

---
+++ js/locales/bootstrap-datepicker.ar.js
@@ -0,0 +1,14 @@
+/**
+ * Arabic translation for bootstrap-datepicker
+ * Mohammed Alshehri <alshehri866@gmail.com>
+ */
+;(function($){
+  $.fn.datepicker.dates['en'] = {
+	    days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+	    daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+	    daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+	    months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+	    monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+	    today: "Today"
+	};
+}(jQuery));
