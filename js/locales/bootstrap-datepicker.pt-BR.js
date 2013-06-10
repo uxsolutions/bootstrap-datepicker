66e0f26b0b4f0a337bcea6efe50f32ab2b8ff90e
Joao Matos Silva <kappy@acydburne.com.pt>
2013-06-10 22:59:21 +0100

    fixed typo

--- js/locales/bootstrap-datepicker.pt-BR.js
+++ js/locales/bootstrap-datepicker.pt-BR.js
@@ -10,6 +10,6 @@
 		months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
 		monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
 		today: "Hoje",
-		clear; "Limpar"
+		clear: "Limpar"
 	};
 }(jQuery));
