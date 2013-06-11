344b7beae51b037dd60c815487945d324db74a7e
Adam Brett <adam@adambrett.co.uk>
2013-06-11 15:32:01 +0100

    Add: New event for calendar redraw/fill
    Included a new event `onFill` for whenever the fill method is called.
    
    This is useful for hooking into the calendar draw process to attach
    custom classes or functionality that might need to be re-attached every
    time the calendar is redrawn.

--- tests/suites/events.js
+++ tests/suites/events.js
@@ -159,3 +159,26 @@ test('Clear button: triggers change and changeDate events', function(){
     equal(triggered_change, 1);
     equal(triggered_changeDate, 1);
 });
+
+test('Fill method: Triggers fill event', function () {
+    this.input = $('<input type="text" value="31-03-2011">')
+                    .appendTo('#qunit-fixture')
+                    .datepicker({
+                        format: "dd-mm-yyyy",
+                        clearBtn: true
+                    })
+                    .focus(); // Activate for visibility checks
+
+    this.dp = this.input.data('datepicker');
+    this.picker = this.dp.picker;
+
+    var triggered = false;
+
+    this.input.on('fill', function (event) {
+        triggered = true;
+    });
+
+    this.dp.fill();
+
+    equal(triggered, true);
+});
