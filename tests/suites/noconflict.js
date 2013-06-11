a9922db962662c968ea231ac5d05a8faadb9d4bd
Frederick John Milens III <fjmilens3@gmail.com>
2013-06-10 21:22:03 -0500

    Refined test case to show issue with noConflict().

--- tests/suites/noconflict.js
+++ tests/suites/noconflict.js
@@ -5,11 +5,12 @@ module('NoConflict', {
     }
 });
 
-test('Datepicker starts after calling noConflict()', function(){
+test('Datepicker starts after calling noConflict() (no undefined defaults or locale_opts)', function(){
     $('<div class="input-append date" id="datepicker">'+
         '<input size="16" type="text" value="12-02-2012" readonly>'+
         '<span class="add-on"><i class="icon-th"></i></span>'+
         '</div>')
         .appendTo('#qunit-fixture')
         .bootstrapDP();
+    expect(0);
 });
