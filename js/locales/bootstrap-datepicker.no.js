a236cd049cda8f3c52dcfbd0bc2a6e34d0115396
Pawel Pierzchala <pawelpierzchala@gmail.com>
2013-06-13 12:25:32 +0200

    Add Norwegian translations

---
+++ js/locales/bootstrap-datepicker.no.js
@@ -0,0 +1,14 @@
+/**
+ *  Norwegian translation for bootstrap-datepicker
+ **/
+;(function($){
+  $.fn.datepicker.dates['no'] = {
+    days: ['Søndag','Mandag','Tirsdag','Onsdag','Torsdag','Fredag','Lørdag'],
+    daysShort: ['Søn','Man','Tir','Ons','Tor','Fre','Lør'],
+    daysMin: ['Sø','Ma','Ti','On','To','Fr','Lø'],
+    months: ['Januar','Februar','Mars','April','Mai','Juni','Juli','August','September','Oktober','November','Desember'],
+    monthsShort: ['Jan','Feb','Mar','Apr','Mai','Jun','Jul','Aug','Sep','Okt','Nov','Des'],
+    today: 'I dag',
+    weekStart: 0
+  };
+}(jQuery));
