386f6c97aa649cc780ea61575954fb55a6e254fb
Andrew Rowls <andrew@eternicode.com>
2013-08-04 18:40:10 -0400

    Remove dependency on glyphicons
    Closes #408

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -1319,9 +1319,9 @@
 		},
 		headTemplate: '<thead>'+
 							'<tr>'+
-								'<th class="prev"><i class="icon-arrow-left"/></th>'+
+								'<th class="prev">&laquo;</th>'+
 								'<th colspan="5" class="datepicker-switch"></th>'+
-								'<th class="next"><i class="icon-arrow-right"/></th>'+
+								'<th class="next">&raquo;</th>'+
 							'</tr>'+
 						'</thead>',
 		contTemplate: '<tbody><tr><td colspan="7"></td></tr></tbody>',
