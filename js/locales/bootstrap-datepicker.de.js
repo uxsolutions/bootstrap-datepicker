60a1a0e351158f914fba810bcfb1b1b7ebb5bfcf
Stephen Reindl <stephen@think2infinity.com>
2013-08-15 11:50:29 +0200

    Updated German translation

--- js/locales/bootstrap-datepicker.de.js
+++ js/locales/bootstrap-datepicker.de.js
@@ -10,6 +10,7 @@
 		months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
 		monthsShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
 		today: "Heute",
+		clear: "Löschen",
 		weekStart: 1,
 		format: "dd.mm.yyyy"
 	};
