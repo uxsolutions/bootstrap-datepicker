9fd772fb8934e1b3592aa8492c92469ce7d20410
renhammington <renhammington@gmail.com>
2013-09-29 14:07:35 +1300

    Added component support for Bootstrap 3.0.0
    Bootstrap has changed how input groups are declared. This change adds ".input-group-addon" as a trigger for opening the datepicker.

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -43,7 +43,7 @@
 		this.element = $(element);
 		this.isInline = false;
 		this.isInput = this.element.is('input');
-		this.component = this.element.is('.date') ? this.element.find('.add-on, .btn') : false;
+		this.component = this.element.is('.date') ? this.element.find('.add-on, .input-group-addon, .btn') : false;
 		this.hasInput = this.component && this.element.find('input').length;
 		if(this.component && this.component.length === 0)
 			this.component = false;
