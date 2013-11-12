8fcfedc873bce596fc08909161564be6eb2d70b2
Gavyn McKenzie <gavyn@etchapps.com>
2013-11-12 17:05:01 +0000

    Added maxViewMode to options

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -137,6 +137,20 @@
 					o.minViewMode = 0;
 			}
 
+			switch (o.maxViewMode) {
+				case 0:
+				case 'days':
+					o.maxViewMode = 0;
+					break;
+				case 1:
+				case 'months':
+					o.maxViewMode = 1;
+					break;
+				default:
+					o.maxViewMode = 2;
+			}
+
+			o.startView = Math.min(o.startView, o.maxViewMode);
 			o.startView = Math.max(o.startView, o.minViewMode);
 
 			o.weekStart %= 7;
@@ -998,7 +1012,7 @@
 
 		showMode: function(dir) {
 			if (dir) {
-				this.viewMode = Math.max(this.o.minViewMode, Math.min(2, this.viewMode + dir));
+				this.viewMode = Math.max(this.o.minViewMode, Math.min(2, this.viewMode + dir, this.o.maxViewMode));
 			}
 			/*
 				vitalets: fixing bug of very special conditions:
@@ -1147,6 +1161,7 @@
 		keyboardNavigation: true,
 		language: 'en',
 		minViewMode: 0,
+		maxViewMode: 2,
 		orientation: "auto",
 		rtl: false,
 		startDate: -Infinity,
