f8238fea07671718554656a5114476fee7a5b654
Illimar Tambek <illimar.tambek@gmail.com>
2013-11-20 13:26:10 +0200

    Fix identation

--- js/locales/bootstrap-datepicker.et.js
+++ js/locales/bootstrap-datepicker.et.js
@@ -4,15 +4,15 @@
  * Fixes by Illimar Tambek <<https://github.com/ragulka>
  */
 ;(function($){
-  $.fn.datepicker.dates['et'] = {
-    days: ["Pühapäev", "Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev", "Pühapäev"],
-    daysShort: ["Pühap", "Esmasp", "Teisip", "Kolmap", "Neljap", "Reede", "Laup", "Pühap"],
-    daysMin: ["P", "E", "T", "K", "N", "R", "L", "P"],
-    months: ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"],
-    monthsShort: ["Jaan", "Veebr", "Märts", "Apr", "Mai", "Juuni", "Juuli", "Aug", "Sept", "Okt", "Nov", "Dets"],
-    today: "Täna",
-    today: "Tühjenda",
-    weekStart: 1,
-    format: "dd.mm.yyyy"
-  };
+	$.fn.datepicker.dates['et'] = {
+		days: ["Pühapäev", "Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev", "Pühapäev"],
+		daysShort: ["Pühap", "Esmasp", "Teisip", "Kolmap", "Neljap", "Reede", "Laup", "Pühap"],
+		daysMin: ["P", "E", "T", "K", "N", "R", "L", "P"],
+		months: ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"],
+		monthsShort: ["Jaan", "Veebr", "Märts", "Apr", "Mai", "Juuni", "Juuli", "Aug", "Sept", "Okt", "Nov", "Dets"],
+		today: "Täna",
+		today: "Tühjenda",
+		weekStart: 1,
+		format: "dd.mm.yyyy"
+	};
 }(jQuery));
