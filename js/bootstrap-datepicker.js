12fcc4204dbdb652f5b12c42dece6b2fc6b2efe4
fadomire <fadomire@gmail.com>
2013-11-26 14:03:38 +0100

    indent formating

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -421,10 +421,10 @@
 				scrollTop = $window.scrollTop();
 
 			var parentsZindex = [];
-		        this.element.parents().each(function() {
-		          var itemZIndex = $(this).css('z-index');
-		          if ( itemZIndex != 'auto' && itemZIndex !== 0 ) parentsZindex.push( parseInt( itemZIndex ) );
-		        });
+			this.element.parents().each(function() {
+				var itemZIndex = $(this).css('z-index');
+		         	if ( itemZIndex != 'auto' && itemZIndex !== 0 ) parentsZindex.push( parseInt( itemZIndex ) );
+		    	});
 			var offset = this.component ? this.component.parent().offset() : this.element.offset();
 			var height = this.component ? this.component.outerHeight(true) : this.element.outerHeight(false);
 			var width = this.component ? this.component.outerWidth(true) : this.element.outerWidth(false);
