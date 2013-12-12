5348e89f225b6ef33109f9fd616953ec76fdbd3f
Michael Hellein <michael.hellein@dealer.com>
2013-12-12 15:17:19 -0500

    Release v1.2.0

--- lib/bootstrap-datepicker/version.rb
+++ lib/bootstrap-datepicker/version.rb
@@ -2,7 +2,7 @@ module Bootstrap
   module Datepicker
     # This is updated via the `grunt bump` command, which has a pretty 
     # unflexible matching syntax.
-    VERSION_STRING = "'version': '1.1.1'"
+    VERSION_STRING = "'version': '1.2.0'"
     # Then, just the version.
     VERSION = VERSION_STRING.match(/\d+\.\d+\.\d+/)[0]
   end
