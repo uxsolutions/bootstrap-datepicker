fa955dcb5be38c4153f10c099622958325ad3625
Ulf Urdén <ulf.urden@dewire.com>
2013-12-18 14:39:48 +0100

    Fixed incorrect grammar in swedish translation.

--- js/locales/bootstrap-datepicker.sv.js
+++ js/locales/bootstrap-datepicker.sv.js
@@ -9,7 +9,7 @@
 		daysMin: ["Sö", "Må", "Ti", "On", "To", "Fr", "Lö", "Sö"],
 		months: ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"],
 		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"],
-		today: "I Dag",
+		today: "Idag",
 		format: "yyyy-mm-dd",
 		weekStart: 1
 	};
