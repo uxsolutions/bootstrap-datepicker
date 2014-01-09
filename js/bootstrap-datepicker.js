d926c20f3db7e2f635075faeb1b026da07195b48
gareth.saunders <celeriacx2@gmail.com>
2014-01-09 16:26:56 +0000

    Month lookups from text input are now case-insensitive. This should be a boon for ease of textual input (case is easily mixed).
    The case of the month itself is unchanged (input text is replaced on
    successful update).

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -1532,7 +1532,7 @@
 			function match_part(){
 				var m = this.slice(0, parts[i].length),
 					p = parts[i].slice(0, m.length);
-				return m === p;
+				return m.toLocaleLowerCase() === p.toLocaleLowerCase();
 			}
 			if (parts.length === fparts.length){
 				var cnt;
