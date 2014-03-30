271a0ffa279a664e8f6cfdc31f70be6b9b6566e4
Andres Kalle <mjomble@gmail.com>
2014-03-30 14:44:52 +0300

    Use maximum z-index among parents to calculate new one

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -547,9 +547,9 @@
 				windowHeight = $window.height(),
 				scrollTop = $window.scrollTop();
 
-			var zIndex = parseInt(this.element.parents().filter(function(){
-					return $(this).css('z-index') !== 'auto';
-				}).first().css('z-index'))+10;
+			var zIndex = Math.max.apply(null, this.element.parents().map(function() {
+					return parseInt($(this).css('z-index')) || 0;
+				})) + 10;
 			var offset = this.component ? this.component.parent().offset() : this.element.offset();
 			var height = this.component ? this.component.outerHeight(true) : this.element.outerHeight(false);
 			var width = this.component ? this.component.outerWidth(true) : this.element.outerWidth(false);
