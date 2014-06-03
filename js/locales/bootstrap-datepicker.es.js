7c31cf2c773ceb235fb693b820cbfe9802692832
Fernando Montoya <montogeek@gmail.com>
2014-06-03 10:05:51 -0500

    Added clear translation :)
    And thanks for this plugin ^_^

--- js/locales/bootstrap-datepicker.es.js
+++ js/locales/bootstrap-datepicker.es.js
@@ -9,6 +9,7 @@
 		daysMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa", "Do"],
 		months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
 		monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
-		today: "Hoy"
+		today: "Hoy",
+		clear: 'Limpiar'
 	};
 }(jQuery));
