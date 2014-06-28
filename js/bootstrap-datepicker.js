ad890d8d9fdc7a71a96ab9298111bbce02eeefdc
Sascha Nos <mail@acreation.de>
2014-06-28 14:31:28 +0200

    disable date toggle on enter

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -1191,11 +1191,6 @@
 					// As such, its behavior should not be hijacked.
 					break;
 				case 13: // enter
-					focusDate = this.focusDate || this.dates.get(-1) || this.viewDate;
-					this._toggle_multidate(focusDate);
-					dateChanged = true;
-					this.focusDate = null;
-					this.viewDate = this.dates.get(-1) || this.viewDate;
 					this.setValue();
 					this.fill();
 					if (this.picker.is(':visible')){
