1314b0523faa20d7f8dabd9ebc98241f02758863
Stefan Wojcik <wojcikstefan@gmail.com>
2014-10-30 16:08:10 -0700

    focus the input before hiding the picker to avoid its reopening

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -423,6 +423,12 @@
 			if (!this.picker.is(':visible'))
 				return;
 			this.focusDate = null;
+
+			// focus the input before hiding the picker to avoid its re-opening
+			if (this.isInput) {
+				this.element.focus();
+			}
+
 			this.picker.hide().detach();
 			this._detachSecondaryEvents();
 			this.viewMode = this.o.startView;
