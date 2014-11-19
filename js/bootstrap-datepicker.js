770b36e99fc4813b535ef6b5221a60f15fcd5d03
Nizar Noorani <nizar.noorani@gmail.com>
2014-11-19 09:59:19 -0600

    makes date recomputation optional when a semantically invalid date is entered. issue 1156

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -206,7 +206,7 @@
 					if (o.startDate instanceof Date)
 						o.startDate = this._local_to_utc(this._zero_time(o.startDate));
 					else
-						o.startDate = DPGlobal.parseDate(o.startDate, format, o.language);
+						o.startDate = DPGlobal.parseDate(o.startDate, format, o.language, o.autoCompute);
 				}
 				else {
 					o.startDate = -Infinity;
@@ -217,7 +217,7 @@
 					if (o.endDate instanceof Date)
 						o.endDate = this._local_to_utc(this._zero_time(o.endDate));
 					else
-						o.endDate = DPGlobal.parseDate(o.endDate, format, o.language);
+						o.endDate = DPGlobal.parseDate(o.endDate, format, o.language, o.autoCompute);
 				}
 				else {
 					o.endDate = Infinity;
@@ -507,6 +507,11 @@
 			else {
 				this.element.val(formatted).change();
 			}
+			if(this.o.invalidDate && formatted == ''){
+				this._trigger('invalidDate');
+			}
+			
+			this.o.invalidDate = false
 		},
 
 		getFormattedDate: function(format){
@@ -630,8 +635,13 @@
 			}
 
 			dates = $.map(dates, $.proxy(function(date){
-				return DPGlobal.parseDate(date, this.o.format, this.o.language);
+				return DPGlobal.parseDate(date, this.o.format, this.o.language, this.o.autoCompute);
 			}, this));
+			
+			if(!dates[0]) {
+				this.o.invalidDate = true;
+			}
+			
 			dates = $.grep(dates, $.proxy(function(date){
 				return (
 					date < this.o.startDate ||
@@ -1408,7 +1418,8 @@
 		startView: 0,
 		todayBtn: false,
 		todayHighlight: false,
-		weekStart: 0
+		weekStart: 0,
+		autoCompute: true
 	};
 	var locale_opts = $.fn.datepicker.locale_opts = [
 		'format',
@@ -1463,7 +1474,7 @@
 			}
 			return {separators: separators, parts: parts};
 		},
-		parseDate: function(date, format, language){
+		parseDate: function(date, format, language, autoCompute){
 			if (!date)
 				return undefined;
 			if (date instanceof Date)
@@ -1557,14 +1568,29 @@
 					}
 					parsed[part] = val;
 				}
-				var _date, s;
-				for (i=0; i < setters_order.length; i++){
-					s = setters_order[i];
-					if (s in parsed && !isNaN(parsed[s])){
-						_date = new Date(date);
-						setters_map[s](_date, parsed[s]);
-						if (!isNaN(_date))
-							date = _date;
+				if(!autoCompute){
+					function parseDateString(str) {
+						var t = str.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/);
+						if(t!==null){
+						  var d=+t[2], m=+t[1], y=+t[3];
+						  var date = new Date(y,m-1,d);
+						  if(date.getFullYear()===y && date.getMonth()===m-1){
+						    return date;   
+						  }
+						}
+						return null;
+					}
+					date = parseDateString(parsed.mm + "/" + parsed.dd + "/" + parsed.yyyy)
+				} else {
+					var _date, s;
+					for (i=0; i < setters_order.length; i++){
+						s = setters_order[i];
+						if (s in parsed && !isNaN(parsed[s])){
+							_date = new Date(date);
+							setters_map[s](_date, parsed[s]);
+							if (!isNaN(_date))
+								date = _date;
+						}
 					}
 				}
 			}
