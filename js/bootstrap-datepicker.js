6c44b176f0edf24d02206c481835c067af5eadcf
Jeroen Thora <jeroenthora@gmail.com>
2015-01-17 15:45:50 +0100

    Fixed little issue with placement of the picker caused in #1227

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -602,7 +602,7 @@
 			var height = this.component ? this.component.outerHeight(true) : this.element.outerHeight(false);
 			var width = this.component ? this.component.outerWidth(true) : this.element.outerWidth(false);
 			var left = offset.left - appendOffset.left,
-				top = offset.top - appendOffset.top + scrollTop;
+				top = offset.top - appendOffset.top;
 
 			this.picker.removeClass(
 				'datepicker-orient-top datepicker-orient-bottom '+
