ef839d5533d714177eb28dbe3ae0466b53f8b67f
Jeroen Thora <jeroenthora@gmail.com>
2015-03-09 20:48:35 +0100

    Generated clean dist files for master

--- dist/js/bootstrap-datepicker.js
+++ dist/js/bootstrap-datepicker.js
@@ -1,5 +1,5 @@
 /*!
- * Datepicker for Bootstrap v1.4.0 (https://github.com/eternicode/bootstrap-datepicker)
+ * Datepicker for Bootstrap v1.5.0 (https://github.com/eternicode/bootstrap-datepicker)
  *
  * Copyright 2012 Stefan Petre
  * Improvements by Andrew Rowls
@@ -1758,7 +1758,7 @@
 
 	/* DATEPICKER VERSION
 	 * =================== */
-	$.fn.datepicker.version =  "1.4.0";
+	$.fn.datepicker.version =  "1.5.0";
 
 	/* DATEPICKER DATA-API
 	* ================== */
