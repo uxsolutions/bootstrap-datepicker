67a2482691d6c253064d44a47f83cde7b21cd6e3
Chris Hemp <chris@chrishemp.com>
2015-04-13 09:45:23 -0400

    Problem: Our translation team prefers the usage of [month number]月 for month selection as this is more commonly seen (one big example is the Windows date-time picker). This also allows better spacing for November and December which are 3 characters long in zh-cn and they run together.  Also the title text was built with string concatenation for month + year and does not leverage a localization format.
    Solution: Switch monthsShort for zh-cn/zh-tw to use monthsShort from moment.js. Add Korean language support by porting from moment as well. In addition a titleFormat key has been added for the .datepicker-switch header of the date picker. This text used to be created with string concatenation and did not work well for Chinese and Korean. Chinese and Korean provide a titleFormat key override and this key defaults to "MM yyyy" for English and other languages.

--- js/locales/bootstrap-datepicker.ja.js
+++ js/locales/bootstrap-datepicker.ja.js
@@ -11,6 +11,7 @@
 		monthsShort: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
 		today: "今日",
 		format: "yyyy/mm/dd",
+		titleFormat: "yyyy年mm月",
 		clear: "クリア"
 	};
 }(jQuery));
