066e835d34495aa06d3c352a7d98ce5a251b4531
Johan Kim <hiphapis@gmail.com>
2015-05-26 20:25:52 +0900

    remove compatibility (kr)

--- js/locales/bootstrap-datepicker.ko.js
+++ js/locales/bootstrap-datepicker.ko.js
@@ -10,6 +10,4 @@
 		months: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
 		monthsShort: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
 	};
-  // for compatibility
-  $.fn.datepicker.dates['kr'] = $.fn.datepicker.dates['ko'];
 }(jQuery));
