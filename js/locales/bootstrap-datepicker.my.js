beddfc0eb97d5722d3418ceaaf748955227037b9
nasrulhazim <nasrulhazim.m@gmail.com>
2015-06-25 10:59:26 +0800

    Added Language Malay

---
+++ js/locales/bootstrap-datepicker.my.js
@@ -0,0 +1,9 @@
+$.fn.datepicker.dates['my'] = {
+    days: ["Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu"],
+    daysShort: ["Ahd", "Isn", "Sel", "Rab", "Kha", "Jum", "Sab"],
+    daysMin: ["Ah", "Is", "Sl", "Rb", "Kh", "Jm", "Sa"],
+    months: ["Januari", "Februari", "Mac", "April", "Mei", "Jun", "Julai", "Ogos", "September", "Oktober", "November", "Disember"],
+    monthsShort: ["Jan", "Feb", "Mac", "Apr", "Mei", "Jun", "Jul", "Ogo", "Sep", "Okt", "Nov", "Dis"],
+    today: "Hari Ini",
+    clear: "Bersihkan"
+};
