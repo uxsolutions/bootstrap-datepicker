647ba5b17558a8735144202724ace63602e4fcbe
Fernando Montoya <montogeek@gmail.com>
2015-12-13 13:44:50 -0500

    Update bootstrap-datepicker.es.js

--- js/locales/bootstrap-datepicker.es.js
+++ js/locales/bootstrap-datepicker.es.js
@@ -10,6 +10,7 @@
 		months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
 		monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
 		today: "Hoy",
+		monthsTitle: "Meses",
 		clear: "Borrar",
 		weekStart: 1,
 		format: "dd/mm/yyyy"
