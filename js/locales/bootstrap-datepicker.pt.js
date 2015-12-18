457c7eda73ed62fa4f63ffe50ff69257aa7646e8
Jeroen Thora <jeroenthora@gmail.com>
2015-12-18 22:40:57 +0100

    Some extra monthTitle translations

--- js/locales/bootstrap-datepicker.pt.js
+++ js/locales/bootstrap-datepicker.pt.js
@@ -11,6 +11,7 @@
 		months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
 		monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
 		today: "Hoje",
+		monthsTitle: "Meses",
 		clear: "Limpar",
 		format: "dd/mm/yyyy"
 	};
