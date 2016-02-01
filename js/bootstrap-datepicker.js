bd67c7f44e85fbc5e09cb13445236033b56ab66c
Jeroen Thora <jeroenthora@gmail.com>
2016-02-01 20:19:00 +0100

    bump version

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -2117,7 +2117,7 @@
 
 	/* DATEPICKER VERSION
 	 * =================== */
-	$.fn.datepicker.version = '1.6.0';
+	$.fn.datepicker.version = '1.6.1';
 
 	/* DATEPICKER DATA-API
 	* ================== */
