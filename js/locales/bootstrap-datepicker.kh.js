d7e9207f37995f55c6c88abd6d73cb290cf25d51
Someth Victory <victory.someth@gmail.com>
2016-02-29 21:02:47 +0700

    Create bootstrap-datepicker.km.js, add deprecated message in .kh

--- js/locales/bootstrap-datepicker.kh.js
+++ js/locales/bootstrap-datepicker.kh.js
@@ -1,6 +1,10 @@
 /**
  * Cambodia (Khmer) translation for bootstrap-datepicker
  * Lytay TOUCH <lytaytouch@gmail.com>
+ *
+ * DEPRECATED: This language code 'kh' is deprecated and will be removed in 2.0.
+ * Khmer support is now in a 'km' translation file to follow the ISO language
+ * code - http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
  */
 ;(function($){
 	$.fn.datepicker.dates['kh'] = {
