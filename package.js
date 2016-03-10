f5ba1fb2310d1aad5535e1f80d798308322675c3
roman <rgnevashev@gmail.com>
2016-03-10 19:46:56 +0300

    meteor package

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'eternicode:bootstrap-datepicker',
+  version: '1.5.1',
+  summary: 'A datepicker for bootstrap',
+  git: 'https://github.com/rgnevashev/fullcalendar',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.addFiles([
+    "dist/css/bootstrap-datepicker3.css",
+    "dist/js/bootstrap-datepicker.js"
+  ], 'client');
+});
