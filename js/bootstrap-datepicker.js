3ceb9acad481ac16374fd9048b5af3daed3bc0df
Gregoire Daussin <gregoire.daussin@gmail.com>
2016-05-08 10:36:47 +0200

    Workaround #1876

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -334,6 +334,7 @@
 				o.defaultViewDate = UTCToday();
 			}
 		},
+		_mousedown_target: null,
 		_events: [],
 		_secondaryEvents: [],
 		_applyEvents: function(evs){
@@ -412,6 +413,21 @@
 					blur: $.proxy(function(e){
 						this._focused_from = e.target;
 					}, this)
+				}],
+				// Input: listen for focusout on element
+				[this.element, {
+					focusout: $.proxy(function(){
+						if (!(
+								this.element.is(this._mousedown_target) ||
+								this.element.find(this._mousedown_target).length ||
+								this.picker.is(this._mousedown_target) ||
+								this.picker.find(this._mousedown_target).length ||
+								this.isInline
+							)) {
+							this.hide();
+						}
+						this._mousedown_target = null;
+					}, this)
 				}]
 			);
 
@@ -433,16 +449,8 @@
 				}],
 				[$(document), {
 					mousedown: $.proxy(function(e){
-						// Clicked outside the datepicker, hide it
-						if (!(
-							this.element.is(e.target) ||
-							this.element.find(e.target).length ||
-							this.picker.is(e.target) ||
-							this.picker.find(e.target).length ||
-							this.isInline
-						)){
-							this.hide();
-						}
+						this._mousedown_target = e.target;
+						setTimeout(this._clearMouseDownTarget, 100);
 					}, this)
 				}]
 			];
@@ -483,6 +491,9 @@
 					return DPGlobal.formatDate(date, format, this.o.language);
 				}, this)
 			});
+		},
+		_clearMouseDownTarget: function () {
+			this._mousedown_target = null;
 		},
 
 		show: function(){
