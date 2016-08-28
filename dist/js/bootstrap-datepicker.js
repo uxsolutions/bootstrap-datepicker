b92ba211ff302d4a4a3436f01201d96d8c96c312
Jeroen Thora <jeroenthora@gmail.com>
2016-08-28 17:40:03 +0200

    Generate dist files

--- dist/js/bootstrap-datepicker.js
+++ dist/js/bootstrap-datepicker.js
@@ -682,8 +682,13 @@
 					this.picker.addClass('datepicker-orient-right');
 					left += width - calendarWidth;
 				} else {
-					// Default to left
-					this.picker.addClass('datepicker-orient-left');
+					if (this.o.rtl) {
+						// Default to right
+						this.picker.addClass('datepicker-orient-right');
+					} else {
+						// Default to left
+						this.picker.addClass('datepicker-orient-left');
+					}
 				}
 			}
 
@@ -1558,6 +1563,7 @@
 		},
 		destroy: function(){
 			$.map(this.pickers, function(p){ p.destroy(); });
+			$(this.inputs).off('changeDate', this.dateUpdated);
 			delete this.element.data().datepicker;
 		},
 		remove: alias('destroy')
