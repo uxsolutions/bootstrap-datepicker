13885397deb748bfc02efe9752f16d06b682ffc7
Kyle Thielk <kylethielk@gmail.com>
2016-09-17 00:48:14 +1000

    resolves bug on days when DST is added.

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -528,7 +528,17 @@
 		},
 
 		_utc_to_local: function(utc){
-			return utc && new Date(utc.getTime() + (utc.getTimezoneOffset()*60000));
+
+			if (!utc) return utc;
+
+			var local = new Date(utc.getTime() + (utc.getTimezoneOffset() * 60000));
+
+			if (local.getTimezoneOffset() != utc.getTimezoneOffset())
+			{
+				local = new Date(utc.getTime() + (local.getTimezoneOffset() * 60000));
+			}
+
+			return utc && local;
 		},
 		_local_to_utc: function(local){
 			return local && new Date(local.getTime() - (local.getTimezoneOffset()*60000));
