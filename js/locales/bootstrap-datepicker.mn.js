d2c8ab2026390e2de0dfc95b6328f3de7a7a0766
Bilguun Ochirbat <bilguun0203@gmail.com>
2017-08-03 12:03:25 +0900

    Update bootstrap-datepicker.mn.js

--- js/locales/bootstrap-datepicker.mn.js
+++ js/locales/bootstrap-datepicker.mn.js
@@ -1,6 +1,6 @@
 /**
  * Mongolian translation for bootstrap-datepicker
- * Andrey Torsunov <andrey.torsunov@gmail.com>
+ * Bilguun Ochirbat <bilguun0203@gmail.com>
  */
 ;(function($){
     $.fn.datepicker.dates['mn'] = {
@@ -9,9 +9,10 @@
         daysMin: ["Ня", "Да", "Мя", "Лх", "Пү", "Ба", "Бя"],
         months: ["Хулгана", "Үхэр", "Бар", "Туулай", "Луу", "Могой", "Морь", "Хонь", "Бич", "Тахиа", "Нохой", "Гахай"],
         monthsShort: ["Хул", "Үхэ", "Бар", "Туу", "Луу", "Мог", "Мор", "Хон", "Бич", "Тах", "Нох", "Гах"],
+        monthsTitle: "Сар",
         today: "Өнөөдөр",
-        clear: "Тодорхой",
-        format: "yyyy.mm.dd",
+        clear: "Цэвэрлэх",
+        format: "yyyy-mm-dd",
         weekStart: 1
     };
 }(jQuery));
