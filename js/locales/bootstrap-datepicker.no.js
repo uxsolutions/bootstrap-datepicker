bf1178c32f59e723262c205eb49e76732ee9aa3d
Oscar S Kolsrud <30559109+OscarKolsrud@users.noreply.github.com>
2019-07-22 13:05:54 +0200

    Added large initial captital

--- js/locales/bootstrap-datepicker.no.js
+++ js/locales/bootstrap-datepicker.no.js
@@ -1,15 +1,14 @@
 /**
  * Norwegian translation for bootstrap-datepicker
- * George Gooding <george@nettsentrisk.no>
  */
 ;(function($){
   $.fn.datepicker.dates['no'] = {
-    days: ['søndag', 'mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag'],
-    daysShort: ['søn', 'man', 'tir', 'ons', 'tor', 'fre', 'lør'],
-    daysMin: ['sø', 'ma', 'ti', 'on', 'to', 'fr', 'lø'],
-    months: ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember'],
-    monthsShort: ['jan', 'feb', 'mar', 'apr', 'mai', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'des'],
-    today: 'i dag',
+    days: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'],
+    daysShort: ['Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør'],
+    daysMin: ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø'],
+    months: ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Des'],
+    today: 'I Dag',
     monthsTitle: 'Måneder',
     clear: 'Nullstill',
     weekStart: 1,
