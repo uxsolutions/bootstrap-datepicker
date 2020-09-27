81c5c9d5b75723e62964382b0a8a7a38cd597d4a
Ardeman <are.theman@gmail.com>
2020-09-27 22:50:01 +0700

    Update bootstrap-datepicker.id.js
    Updated: daysShort, daysMin, monthsShort
    Added: monthsTitle, weekStart, format

--- js/locales/bootstrap-datepicker.id.js
+++ js/locales/bootstrap-datepicker.id.js
@@ -1,15 +1,19 @@
 /**
  * Bahasa translation for bootstrap-datepicker
  * Azwar Akbar <azwar.akbar@gmail.com>
+ * Ardeman <mail@ardeman.com>
  */
 ;(function($){
 	$.fn.datepicker.dates['id'] = {
 		days: ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"],
-		daysShort: ["Mgu", "Sen", "Sel", "Rab", "Kam", "Jum", "Sab"],
-		daysMin: ["Mg", "Sn", "Sl", "Ra", "Ka", "Ju", "Sa"],
+		daysShort: ["Min", "Sen", "Sel", "Rab", "Kam", "Jum", "Sab"],
+		daysMin: ["Mg", "Sn", "Sl", "Rb", "Km", "Jm", "Sb"],
 		months: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"],
-		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Ags", "Sep", "Okt", "Nov", "Des"],
+		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agt", "Sep", "Okt", "Nov", "Des"],
 		today: "Hari Ini",
-		clear: "Kosongkan"
+		monthsTitle: "Bulan",
+		clear: "Kosongkan",
+		weekStart: 0,
+		format: "dd-mm-yyyy"
 	};
 }(jQuery));
