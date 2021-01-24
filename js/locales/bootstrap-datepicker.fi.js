33b31e538b09f8b630369bcf8903c8785f8282c7
Richard Sahrakorpi <rjgunning+github@gmail.com>
2021-01-24 18:18:26 +0200

    minor #2387 Fix Finnish monthShort translations as 3 letter invalid (RGunning, acrobat)
    This PR was squashed before being merged into the 1.10-dev branch.
    
    Discussion
    ----------
    
    The 3 letter monthShort codes are not used in Finnish, so fixed translations to valid shortenings
    
    | Q               | A
    | --------------- | ---
    | Bug fix?        | yes
    | New feature?    | no|yes
    | BC breaks?      | no|yes
    | Related tickets | fixes #X, partially #Y, mentioned in #Z
    | License         | MIT
    
    
    Commits
    -------
    
    bb96dbfc532edf962011a538b7b390eafdf37567 Fix Finnish monthShort translations as 3 letter invalid
    307362cf75f8f22adb6d08ee3cdc7fc6a0a5953f Merge branch 'master' into patch-2

--- js/locales/bootstrap-datepicker.fi.js
+++ js/locales/bootstrap-datepicker.fi.js
@@ -8,7 +8,7 @@
 		daysShort: ["sun", "maa", "tii", "kes", "tor", "per", "lau"],
 		daysMin: ["su", "ma", "ti", "ke", "to", "pe", "la"],
 		months: ["tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu"],
-		monthsShort: ["tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mar", "jou"],
+		monthsShort: ["tammi", "helmi", "maalis", "huhti", "touko", "kesä", "heinä", "elo", "syys", "loka", "marras", "joulu"],
 		today: "tänään",
 		clear: "Tyhjennä",
 		weekStart: 1,
