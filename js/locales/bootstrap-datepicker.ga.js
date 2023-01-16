ec195fc36cdce350bc35947864bb7dc759cae48d
rbrennan <rbrennan@nli.ie>
2023-01-16 17:22:50 +0000

    Add Irish Locale to datepicker

---
+++ js/locales/bootstrap-datepicker.ga.js
@@ -0,0 +1,18 @@
+/**
+ * Irish translation for bootstrap-datepicker
+ * Ronan Brennan <https://github.com/Brennanro4-dot>
+ */
+;(function($){
+$.fn.datepicker.dates['ga'] = {
+  days: ["Dé Domhnaigh", "Dé Luain", "Dé Máirt", "Dé Céadaoin", "Déardaoin", "Dé hAoine", "Dé Sathairn"],
+  daysShort: ["Dom", "Lua", "Mái", "Céa", "Déa", "hAo", "Sat"],
+  daysMin: ["Do", "Lu", "Má", "Cé", "Dé", "hA", "Sa"],
+  months: ["Eanáir", "Feabhra", "Márta", "Aibreán", "Bealtaine", "Meitheamh", "Iúil", "Lúnasa", "Meán Fómhair", "Deireadh Fómhair", "Samhain", "Nollaig"],
+  monthsShort: ["Jan", "Fea", "Már", "Aib", "Bea", "Mei", "Iúi", "Lún", "Meá", "Dei", "Sam", "Nol"],
+  today: "inniu",
+  clear: "Scrios é",
+  format: "dd/MM/yyyy",
+  titleFormat: "MM yyyy",
+  weekStart: 0
+};
+}(jQuery));
