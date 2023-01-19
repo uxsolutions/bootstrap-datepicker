a84c9b0e6a2cfe4ca40f520038a96487b75d3ec0
Ronan Brennan <84009560+Brennanro4-dot@users.noreply.github.com>
2023-01-19 09:09:36 +0000

    Update bootstrap-datepicker.ga.js

--- js/locales/bootstrap-datepicker.ga.js
+++ js/locales/bootstrap-datepicker.ga.js
@@ -8,7 +8,7 @@ $.fn.datepicker.dates['ga'] = {
   daysShort: ["Dom", "Lua", "Mái", "Céa", "Déa", "hAo", "Sat"],
   daysMin: ["Do", "Lu", "Má", "Cé", "Dé", "hA", "Sa"],
   months: ["Eanáir", "Feabhra", "Márta", "Aibreán", "Bealtaine", "Meitheamh", "Iúil", "Lúnasa", "Meán Fómhair", "Deireadh Fómhair", "Samhain", "Nollaig"],
-  monthsShort: ["Jan", "Fea", "Már", "Aib", "Bea", "Mei", "Iúi", "Lún", "Meá", "Dei", "Sam", "Nol"],
+  monthsShort: ["Ean", "Fea", "Már", "Aib", "Bea", "Mei", "Iúi", "Lún", "Meá", "Dei", "Sam", "Nol"],
   today: "inniu",
   clear: "Scrios é",
   format: "dd/MM/yyyy",
