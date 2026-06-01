39af28a9d3264a6dd2adf08ea57220e28c17c237
OSWorX <info@osworx.net>
2026-06-01 10:23:04 +0200

    Create bootstrap-datepicker.de-AT.js
    Translation für Austria

---
+++ js/locales/bootstrap-datepicker.de-AT.js
@@ -0,0 +1,18 @@
+/**
+ * German - Austrian translation for bootstrap-datepicker
+ * mic <mp@eglie.at>
+ */
+;(function($){
+	$.fn.datepicker.dates['de-AT'] = {
+		days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+		daysShort: ["Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam"],
+		daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+		months: ["Jänner", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+		monthsShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+		today: "Heute",
+		monthsTitle: "Monate",
+		clear: "Löschen",
+		weekStart: 1,
+		format: "dd.mm.yyyy"
+	};
+}(jQuery));
