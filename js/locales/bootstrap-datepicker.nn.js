9dbc4d666280c88205c8b9fb3e6317d6ea3e8960
Arvind <120897974+arvindfroi@users.noreply.github.com>
2026-06-28 04:33:30 +0200

    Add Norwegian Nynorsk (nn) locale

---
+++ js/locales/bootstrap-datepicker.nn.js
@@ -0,0 +1,17 @@
+/**
+ * Norwegian Nynorsk translation for bootstrap-datepicker
+ */
+;(function($){
+  $.fn.datepicker.dates['nn'] = {
+    days: ['søndag', 'måndag', 'tysdag', 'onsdag', 'torsdag', 'fredag', 'laurdag'],
+    daysShort: ['søn', 'mån', 'tys', 'ons', 'tor', 'fre', 'lau'],
+    daysMin: ['sø', 'må', 'ty', 'on', 'to', 'fr', 'la'],
+    months: ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember'],
+    monthsShort: ['jan', 'feb', 'mar', 'apr', 'mai', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'des'],
+    today: 'i dag',
+    monthsTitle: 'Månadar',
+    clear: 'Nullstill',
+    weekStart: 1,
+    format: 'dd.mm.yyyy'
+  };
+}(jQuery));
